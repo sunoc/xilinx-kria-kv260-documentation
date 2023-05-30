7610e7045a9d4953be205c1757ae7473b111677c
Vincent Conus <sunoc@pop-os.localdomain>
2023-05-30 16:39:46 +0900

    adding the c++ settings. Need more tweakings

--- src/my_custom_toolchain.cmake
+++ src/my_custom_toolchain.cmake
@@ -7,13 +7,16 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 set(PLATFORM_NAME "LwIP")
 
 # Makefile flags
-set(ARCH_CPU_FLAGS "-mcpu=cortex-r5 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -Wno-dev -Wno-unused-parameter -Wno-unused-value -Wno-unused-variable -Wno-unused-but-set-variable -DUSE_HAL_DRIVER -D_TIMEVAL_DEFINED -O0 -Wall -fdata-sections -ffunction-sections")
+set(ARCH_CPU_FLAGS "-O2 -ffunction-sections -fdata-sections -fno-exceptions -mcpu=cortex-r5 -nostdlib -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -Wno-unused-parameter -Wno-unused-value -Wno-unused-variable -Wno-unused-but-set-variable -DUSE_HAL_DRIVER -D_TIMEVAL_DEFINED -Wall")
 set(ARCH_OPT_FLAGS "")
 
 set(CMAKE_C_COMPILER /armr5-toolchain/bin/armr5-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER /armr5-toolchain/bin/armr5-none-eabi-g++)
 
 set(CMAKE_C_COMPILER_WORKS 1 CACHE INTERNAL "")
+set(CMAKE_CXX_COMPILER_WORKS 1 CACHE INTERNAL "")
 
 set(CMAKE_C_FLAGS_INIT "-std=c11 ${ARCH_CPU_FLAGS} ${ARCH_OPT_FLAGS} -DCLOCK_MONOTONIC=0" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS_INIT "-std=c++11 ${ARCH_CPU_FLAGS} -DCLOCK_MONOTONIC=0 -D'__attribute__(x)='" CACHE STRING "" FORCE)
 
 set(__BIG_ENDIAN__ 0)

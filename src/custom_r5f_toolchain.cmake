bf3ab16173d98c055be21c4e185e2db2d31b86f4
Vincent Conus <sunoc@pop-os.localdomain>
2023-06-01 16:56:29 +0900

    a toolchain cmake file closer to the original example

--- src/custom_r5f_toolchain.cmake
+++ src/custom_r5f_toolchain.cmake
@@ -1,22 +1,20 @@
-include(CMakeForceCompiler)
-
 set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_CROSSCOMPILING 1)
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
-set(PLATFORM_NAME "LwIP")
+set(CMAKE_INSTALL_LIBDIR /usr/)
+
 
 # Makefile flags
-set(ARCH_CPU_FLAGS "-O2 -ffunction-sections -fdata-sections -fno-exceptions -mcpu=cortex-r5 -nostdlib -mthumb -mfpu=vfpv3-d16 -mfloat-abi=hard -Wno-unused-parameter -Wno-unused-value -Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable -DUSE_HAL_DRIVER -D_TIMEVAL_DEFINED  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -Wall")
+set(ARCH_CPU_FLAGS "-O2 -ffunction-sections -fdata-sections -fno-exceptions -ffreestanding -mcpu=cortex-r5 -nostdlib -mthumb -mfpu=vfpv3-d16 -mfloat-abi=hard -Wall --param max-inline-insns-single=500 -DF_CPU=84000000L -D'RCUTILS_LOG_MIN_SEVERITY=RCUTILS_LOG_MIN_SEVERITY_NONE'" CACHE STRING "" FORCE)
 set(ARCH_OPT_FLAGS "")
 
-set(CMAKE_C_COMPILER /armr5-toolchain/bin/armr5-none-eabi-gcc)
-set(CMAKE_CXX_COMPILER /armr5-toolchain/bin/armr5-none-eabi-g++)
+set(CMAKE_C_COMPILER armr5-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER armr5-none-eabi-g++)
 
 set(CMAKE_C_COMPILER_WORKS 1 CACHE INTERNAL "")
 set(CMAKE_CXX_COMPILER_WORKS 1 CACHE INTERNAL "")
 
-set(CMAKE_C_FLAGS_INIT "-std=c11 ${ARCH_CPU_FLAGS} ${ARCH_OPT_FLAGS} -DCLOCK_MONOTONIC=0" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS_INIT "-std=c11 ${ARCH_CPU_FLAGS} ${ARCH_OPT_FLAGS} -DCLOCK_MONOTONIC=0 -D'__attribute__(x)='" CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS_INIT "-std=c++11 ${ARCH_CPU_FLAGS} -DCLOCK_MONOTONIC=0 -D'__attribute__(x)='" CACHE STRING "" FORCE)
 
 set(__BIG_ENDIAN__ 0)
